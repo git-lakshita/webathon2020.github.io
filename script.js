bad60771f2370018a94399c0038de843f71c5b3b
charchit95 <charchit95@gmail.com>
2020-07-25 23:39:48 +0530

    update

--- script.js
+++ script.js
@@ -5,20 +5,15 @@ function allFilled () {
     });
     return filled;
 }
+
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
-$('#myBtn').bind('click', function(e) {
-    e.preventDefault()
+$('#myBtn').bind('click', function(event) {
+    event.preventDefault()
     if(allFilled()) modal.style.display = "block";
 });
-
-btn.onclick = function () {
-    $('#username, #password, #address, #mobile, #email').bind('keyup', function() {
-        if(allFilled()) modal.style.display = "block";
-    });
-}
 
 span.onclick = function () {
     modal.style.display = "none";
